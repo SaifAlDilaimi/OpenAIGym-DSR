8eebf9f1b8a8b093614a29de68500a85319c155d
Saif Al-Dilaimi <Saif.Al-Dilaimi@rub.de>
2021-05-13 13:00:10 +0200

    init

--- dsr_example.py
+++ dsr_example.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 import tensorflow as tf
 import gym
@@ -8,51 +9,121 @@ from statistics import mean
 from gym import wrappers
 
 from tensorflow.keras import Model
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Layer
 from tensorflow.keras.layers import InputLayer
+from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2DTranspose
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.layers import Concatenate
 from tensorflow.keras.optimizers import SGD
 
-class DSRModel(Model):
-    def __init__(self, 
+
+class DSRModel:
+    def __init__(
+        self, 
         num_states: int, 
-        num_actions: int,
-        learning_rate: float
+        num_actions: int, 
+        learning_rate: float,
     ) -> None:
         super(DSRModel, self).__init__()
         self.num_states = num_states
         self.num_actions = num_actions
         self.learning_rate = learning_rate
+        self.num_units = 512
+        self.num_outputs_regression = 1
 
-    def create_feature_model(
-        self,
-        shape: tuple
-    ) -> Model:
-        input_layer = InputLayer(input_shape=shape)
-        x = Conv2D(32, (8, 8), activation='relu')(input_layer)
-        x = Conv2D(64, (4, 4), activation='relu')(x)
-        x = Conv2D(64, (3, 3), activation='relu')(x)
-        features_phi_state = Dense(512, activation='')(x)
-        rewards_state = Dense(self.num_actions, activation='linear')(features_phi_state)
+    def get_feature_extractor(self) -> Sequential:
+        model = Sequential(name="feature_extractor")
+        model.add(
+            Conv2D(
+                filters=8, kernel_size=[3, 3], strides=[2, 2],
+                padding='SAME', input_shape=(10, 10, 1))
+        )
+        model.add(
+            Conv2D(filters=16, kernel_size=[3, 3], strides=[2, 2])
+        )
+        model.add(Flatten())
+        model.add(Dense(units=self.num_units, name="phi_state"))
+        model.add(Dense(units=self.num_outputs_regression, name="reward_regression"))
+        return model
 
-        model = Model(inputs=input_layer, outputs=rewards_state)
-        opt_sgd = SGD(learning_rate=self.learning_rate)
-        model.compile(optimizer=opt_sgd)
+    def get_deconv(self, input_layer: Layer) -> Sequential:
+        model = Sequential(name="deconv_decoder")
+        model.add(InputLayer(input_shape=input_layer.output_shape))
+        model.add(Reshape(target_shape=(8, 8, 4)))
+        model.add(
+            Conv2DTranspose(
+                filters=3, kernel_size=[3, 3], strides=[2, 2]
+            )
+        )
+        model.add(
+            Conv2DTranspose(
+                filters=3, kernel_size=[3, 3], strides=[2, 2]
+            )
+        )
+        model.add(Activation("tanh"))
+        return model
 
-        self.features_phi_state = features_phi_state
+    def add_sr_layer(self, input_layer: Layer):
+        x = Dense(
+            units=self.num_units,
+        )(input_layer)
+        x = Dense(units=256)(x)
+        x = Dense(units=self.num_units)(x)
+        return x
+
+    def get_successor(self, input_layer) -> Sequential:
+        model = Sequential()
+
+        mu_list: List[Dense] = []
+        self.q: List[Dense] = []
+
+        for i in range(self.num_actions):
+            mu_list.append(self.add_sr_layer(input_layer))
+
+            self.q.append(
+                Dense(
+                    units=1
+                )(mu_list[i])
+            )
+
+        stacked = Concatenate(axis=-1)([mu_list])
+        model.add(stacked)
+        # model.add(Reshape(target_shape=()))
+
+        return model
 
 
-    @tf.function
-    def call(self, inputs):
-        z = self.input_layer(inputs)
-        for layer in self.hidden_layers:
-            z = layer(z)
-        output = self.output_layer(z)
-        return output
+    def create_feature_model(self, shape: tuple) -> Model:
+        extractor = self.get_feature_extractor()
+        extractor.summary()
+        phi_state_layer = extractor.get_layer("phi_state")
+
+        deconv = self.get_deconv(input_layer=phi_state_layer)
+        deconv.summary()
+
+        successor = self.get_successor(input_layer=phi_state_layer)
+        successor.summary()
+
+        # model = Model(inputs=[extractor.layers[0]], outputs=[extractor.layers[-1]])
 
 
 class DQN:
-    def __init__(self, num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr):
+    def __init__(
+        self,
+        num_states,
+        num_actions,
+        hidden_units,
+        gamma,
+        max_experiences,
+        min_experiences,
+        batch_size,
+        lr,
+    ):
         self.num_actions = num_actions
         self.batch_size = batch_size
         self.optimizer = tf.optimizers.Adam(lr)
@@ -68,19 +139,27 @@ class DQN:
     def train(self, TargetNet):
         if len(self.experience['s']) < self.min_experiences:
             return 0
-        ids = np.random.randint(low=0, high=len(self.experience['s']), size=self.batch_size)
+        ids = np.random.randint(
+            low=0, high=len(self.experience['s']), size=self.batch_size
+        )
         states = np.asarray([self.experience['s'][i] for i in ids])
         actions = np.asarray([self.experience['a'][i] for i in ids])
         rewards = np.asarray([self.experience['r'][i] for i in ids])
         states_next = np.asarray([self.experience['s2'][i] for i in ids])
         dones = np.asarray([self.experience['done'][i] for i in ids])
         value_next = np.max(TargetNet.predict(states_next), axis=1)
-        actual_values = np.where(dones, rewards, rewards+self.gamma*value_next)
+        actual_values = np.where(
+            dones, rewards, rewards + self.gamma * value_next
+        )
 
         with tf.GradientTape() as tape:
             selected_action_values = tf.math.reduce_sum(
-                self.predict(states) * tf.one_hot(actions, self.num_actions), axis=1)
-            loss = tf.math.reduce_mean(tf.square(actual_values - selected_action_values))
+                self.predict(states) * tf.one_hot(actions, self.num_actions),
+                axis=1,
+            )
+            loss = tf.math.reduce_mean(
+                tf.square(actual_values - selected_action_values)
+            )
         variables = self.model.trainable_variables
         gradients = tape.gradient(loss, variables)
         self.optimizer.apply_gradients(zip(gradients, variables))
@@ -121,7 +200,13 @@ def play_game(env, TrainNet, TargetNet, epsilon, copy_step):
             reward = -200
             env.reset()
 
-        exp = {'s': prev_observations, 'a': action, 'r': reward, 's2': observations, 'done': done}
+        exp = {
+            's': prev_observations,
+            'a': action,
+            'r': reward,
+            's2': observations,
+            'done': done,
+        }
         TrainNet.add_experience(exp)
         loss = TrainNet.train(TargetNet)
         if isinstance(loss, int):
@@ -132,6 +217,7 @@ def play_game(env, TrainNet, TargetNet, epsilon, copy_step):
         if iter % copy_step == 0:
             TargetNet.copy_weights(TrainNet)
     return rewards, mean(losses)
+
 
 def make_video(env, TrainNet):
     env = wrappers.Monitor(env, os.path.join(os.getcwd(), "videos"), force=True)
@@ -149,44 +235,12 @@ def make_video(env, TrainNet):
 
 
 def main():
-    env = gym.make('CartPole-v0')
-    gamma = 0.99
-    copy_step = 25
-    num_states = len(env.observation_space.sample())
-    num_actions = env.action_space.n
-    hidden_units = [200, 200]
-    max_experiences = 10000
-    min_experiences = 100
-    batch_size = 32
-    lr = 1e-2
-    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    log_dir = 'logs/dqn/' + current_time
-    summary_writer = tf.summary.create_file_writer(log_dir)
-
-    TrainNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr)
-    TargetNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr)
-    N = 50000
-    total_rewards = np.empty(N)
-    epsilon = 0.99
-    decay = 0.9999
-    min_epsilon = 0.1
-    for n in range(N):
-        epsilon = max(min_epsilon, epsilon * decay)
-        total_reward, losses = play_game(env, TrainNet, TargetNet, epsilon, copy_step)
-        total_rewards[n] = total_reward
-        avg_rewards = total_rewards[max(0, n - 100):(n + 1)].mean()
-        with summary_writer.as_default():
-            tf.summary.scalar('episode reward', total_reward, step=n)
-            tf.summary.scalar('running avg reward(100)', avg_rewards, step=n)
-            tf.summary.scalar('average loss)', losses, step=n)
-        if n % 100 == 0:
-            print("episode:", n, "episode reward:", total_reward, "eps:", epsilon, "avg reward (last 100):", avg_rewards,
-                  "episode loss: ", losses)
-    print("avg reward for last 100 episodes:", avg_rewards)
-    make_video(env, TrainNet)
-    env.close()
+    num_states = 10
+    num_actions = 8
+    learning_rate = 1e-3
+    m = DSRModel(num_states, num_actions, learning_rate)
+    m.create_feature_model(shape=(10, 10, 1))
 
 
 if __name__ == '__main__':
-    for i in range(3):
-        main()
+    main()
