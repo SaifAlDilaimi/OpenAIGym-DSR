403c72b76ca4d90b2d3eaf361cae8f78707fb543
Saif Al-Dilaimi <Saif.Al-Dilaimi@rub.de>
2021-05-26 15:58:09 +0200

    added env yaml

--- dsr_example.py
+++ dsr_example.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import gym
 import gym_minigrid
 import cv2
+import matplotlib.pyplot as plt
 
 from collections import deque
 from gym import wrappers
@@ -14,7 +15,7 @@ from tensorflow.keras.layers import Conv2DTranspose
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Reshape
-from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import SGD, RMSprop, Adam
 from tensorflow.keras.losses import mean_squared_error
 
 
@@ -23,12 +24,14 @@ class DSRModel:
         self,
         input_shape: tuple,
         num_actions: int,
-        learning_rate: float
+        learning_rate: float,
+        momentum: float
     ) -> None:
         super(DSRModel, self).__init__()
         self.input_shape = input_shape
         self.num_actions = num_actions
         self.learning_rate = learning_rate
+        self.momentum = momentum
 
         self.create_DSR_model()
 
@@ -104,18 +107,18 @@ class DSRModel:
 
     def add_sr_block(self, input_layer: Input, action: int):
         x = Dense(
-            units=512,
+            units=256, 
+            activation='relu'
         )(input_layer)
-        x = Dense(units=256)(x)
-        x = Dense(units=256)(x)
-        x = Dense(units=1, name=f'm_state_a{action}')(x)
+        x = Dense(units=512, activation='relu')(x)
+        x = Dense(units=256, activation='relu', name=f'm_state_a{action}')(x)
         return x
 
     def get_successor(self) -> Model:
         output_layers = []
 
         successor_input = Input(shape=(256,), name="SR_Input")
-        x = Dense(256)(successor_input)
+        x = Dense(512, activation='relu')(successor_input)
 
         for a in range(self.num_actions):
             output_layers.append(self.add_sr_block(x, action=a))
@@ -153,7 +156,10 @@ class DSRModel:
                 'model_r_regression': mean_squared_error,
                 'model_decoder': mean_squared_error
             },
-            optimizer=RMSprop(learning_rate=self.learning_rate)
+            optimizer=RMSprop(
+                learning_rate=self.learning_rate,
+                momentum=self.momentum
+            )
         )
 
         phi_successor = Model(
@@ -169,7 +175,10 @@ class DSRModel:
 
         phi_successor.compile(
             loss=mean_squared_error,
-            optimizer=RMSprop(learning_rate=self.learning_rate)
+            optimizer=RMSprop(
+                learning_rate=self.learning_rate,
+                momentum=self.momentum
+            )
         )
 
         
@@ -198,78 +207,158 @@ class DQN:
         epsilon_decay: float = 0.995,
         batch_size: int = 32,
         lr: float = 2.5e-4,
+        momentum: float = 0.95,
         memory_size: int = 1000000
     ) -> float:
         self.action_space = action_space
         self.observation_shape = observation_shape
-        self.batch_size = batch_size
         self.gamma = gamma
-        self.lr = lr
         self.epsilon = epsilon
         self.epsilon_min = epsilon_min
         self.epsilon_decay = epsilon_decay
+        self.batch_size = batch_size
+        self.lr = lr
+        self.momentum = momentum
         self.memory  = deque(maxlen=memory_size)
+        self.nonzero_memory  = deque(maxlen=int(5e4))
         self.tau = .125
 
         dsr = DSRModel(
             input_shape=observation_shape, 
             num_actions=action_space, 
-            learning_rate=lr
+            learning_rate=lr,
+            momentum=momentum
         )
         dsr_target = DSRModel(
             input_shape=observation_shape, 
             num_actions=action_space, 
-            learning_rate=lr
+            learning_rate=lr,
+            momentum=momentum
         )
         self.model_reward_deconv = dsr.reward_deconv
         self.model_sr = dsr.sr
         self.target_model_reward_deconv = dsr_target.reward_deconv
         self.target_model_sr = dsr_target.sr
 
-    def act(self, state):
-        self.epsilon *= self.epsilon_decay
-        self.epsilon = max(self.epsilon_min, self.epsilon)
 
+    def calculate_a_prime(
+        self,
+        new_state: np.ndarray
+    ) -> int:
+        batch = np.array([new_state])
+
+        successor_features_next_state = self.model_sr.predict_on_batch(batch)
+        w = self.calculate_w()
+
+        Q_s = np.matmul(successor_features_next_state, w)[0]
+        a_prime = np.argmax(Q_s)
+
+        return a_prime
+
+    def calculate_w(
+        self
+    ) -> np.ndarray:
+        feature_branch = self.model_reward_deconv.get_layer("feature_branch")
+        phi_state_weights = feature_branch.get_layer("phi_state").get_weights()
+        w = phi_state_weights[0][1] # 0 = weights (not bais), 1 = output weights
+        return w
+
+    def calculate_phi(
+        self,
+        state: np.ndarray
+    ) -> np.ndarray:
+        feature_branch = self.model_reward_deconv.get_layer("feature_branch")
+        phi_input = feature_branch.input
+        phi_output = feature_branch.get_layer("phi_state").output
+        
+        phi_layer_model = Model(inputs=phi_input, outputs=phi_output)
+        phi_state = phi_layer_model(np.array([state])).numpy()
+
+        return phi_state
+
+    def calculate_target_r(
+        self,
+        phi_state: np.ndarray
+    ) -> np.ndarray:
+        w = self.calculate_w()
+        target_r = np.matmul(phi_state, w)
+        return target_r
+
+    def act(self, state):
         if np.random.random() < self.epsilon:
             return np.random.choice(self.action_space)
 
         batch = np.array([state])
-        m_s_a = self.model_sr.predict(batch)
-        a = np.argmax(m_s_a)
+
+        successor_features = self.model_sr.predict_on_batch(batch)[0]
+        w = self.calculate_w()
+
+        Q_s = np.matmul(successor_features, w)
+        print(Q_s)
+        a = np.argmax(Q_s)
 
         return a
 
     def remember(self, state, action, reward, new_state, done):
-        self.memory.append([state, action, reward, new_state, done])
+        if reward == 10:
+            self.nonzero_memory.append([state, action, reward, new_state, done])
+        else:
+            self.memory.append([state, action, reward, new_state, done])
+
+    def sample_batch(
+        self
+    ) -> list:
+        memory = self.memory
+
+        #if np.random.random() <= 0.3:
+        #    memory = self.nonzero_memory
+        
+        sample_indicies = np.random.choice(len(memory), self.batch_size)
+        samples = [memory[idx] for idx in sample_indicies]
+
+        return samples
+
+    def memory_ready(self) -> bool:
+        if (
+            len(self.memory) < self.batch_size * 2 
+            or len(self.nonzero_memory) < self.batch_size * 2
+        ): 
+            return False
+        else:
+            return True
 
     def replay(self):
-        if len(self.memory) < self.batch_size: 
-            return
+        #if not self.memory_ready():
+        #    return
 
-        sample_indicies = np.random.choice(len(self.memory), self.batch_size)
-        samples = [self.memory[idx] for idx in sample_indicies]
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+
+        samples = self.sample_batch()
 
         for sample in samples:
             state, action, reward, new_state, done = sample
             batch_state = np.array([state])
-            target = self.target_model_sr.predict(batch_state)
+            batch_new_state = np.array([new_state])
 
+            phi_state = self.calculate_phi(state)
+            target_sf_next_state = self.target_model_sr.predict_on_batch(batch_new_state)
+            a_prime = self.calculate_a_prime(new_state)
+            target_sf_next_state[0][a_prime] *= self.gamma
+            target_sf_next_state[0][a_prime] += phi_state
+
+            target_r = self.calculate_target_r(phi_state)
+            #print("T_r: ", target_r)
             if done:
-                target[0][action] = reward
-            else:
-                batch_new_state = np.array([new_state])
-                Q_future = max(self.target_model_sr.predict(batch_new_state)[0])
-                target[0][action] = reward + Q_future * self.gamma
+                target_r = reward
 
-            self.model_sr.fit(batch_state, target, epochs=1, verbose=0)
-            self.model_reward_deconv.fit(
+            self.model_sr.train_on_batch(batch_state, target_sf_next_state)
+            self.model_reward_deconv.train_on_batch(
                 x=batch_state, 
                 y={
-                    'model_r_regression': np.array([reward]),
+                    'model_r_regression': np.array([target_r]),
                     'model_decoder': batch_state
-                },
-                epochs=1,
-                verbose=0
+                }
             )
 
     def target_train(self):
@@ -291,58 +380,75 @@ def resize_img(img: np.array) -> np.array:
 
     # resize image
     output = cv2.resize(img, dsize, interpolation = cv2.INTER_AREA)
+    output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+    output = np.reshape(output, newshape=(64, 64, 1))
     return output
 
-
-def main():
-    env = gym.make('MiniGrid-Empty-8x8-v0')
-    env = RGBImgPartialObsWrapper(env) # Get pixel observations
-    env = ImgObsWrapper(env) # Get rid of the 'mission' field
-    gamma = 0.99
-    num_actions = env.action_space.n
-    img_shape = (64, 64, 3)
-    batch_size = 32
-    lr = 1e-2
-    trials  = 1000
-    trial_len = 500
-
-    dqn_agent = DQN(
-        action_space=num_actions,
-        observation_shape=img_shape,
-        gamma=gamma,
-        batch_size=batch_size, 
-        lr=lr
-    )
-    
-    steps = []
-    for trial in range(trials):
-        print(f'Trail {trial}/{trials}')
+def collect_nonzero_samples(env, dqn_agent: DQN):
+    trial_len = 255
+    while not dqn_agent.memory_ready():
         cur_state = env.reset()
         cur_state = resize_img(cur_state)
-        for step in range(trial_len):
-            print(f'Step {step}/{trial_len}', end="\r")
+        for _ in range(trial_len):
             action = dqn_agent.act(cur_state)
             new_state, reward, done, _ = env.step(action)
             new_state = resize_img(new_state)
 
-            dqn_agent.remember(cur_state, action, reward, new_state, done)
+            print(f'Collecting Memory {len(dqn_agent.nonzero_memory)}/{dqn_agent.batch_size*2}', end="\r")
             
+            dqn_agent.remember(cur_state, action, reward, new_state, done)
+
             dqn_agent.replay()       # internally iterates default (prediction) model
             dqn_agent.target_train() # iterates target model
 
             cur_state = new_state
             if done:
                 break
-        if step >= 199:
-            print("Failed to complete in trial {}".format(trial))
-            if step % 10 == 0:
-                dqn_agent.save_model("trial-{}.model".format(trial))
-        else:
-            print("Completed in {} trials".format(trial))
-            dqn_agent.save_model("success.model")
-            break
 
+def main():
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    gamma = 0.99
+    num_actions = 3
+    img_shape = (64, 64, 1)
+    batch_size = 32
+    lr = 2.5e-4
+    momentum = 0.95
+    trials  = 1000
+    trial_len = 255
 
+    dqn_agent = DQN(
+        action_space=num_actions,
+        observation_shape=img_shape,
+        gamma=gamma,
+        batch_size=batch_size, 
+        lr=lr,
+        momentum=momentum
+    )
+    
+    #collect_nonzero_samples(env, dqn_agent)
+
+    for trial in range(trials):
+        print(f'Trail {trial}/{trials}')
+        cur_state = env.reset()
+        cur_state = resize_img(cur_state)
+        for step in range(trial_len):
+            action = dqn_agent.act(cur_state)
+            new_state, reward, done, _ = env.step(action)
+            new_state = resize_img(new_state)
+            
+            print(f'Step {step}/{trial_len}; Agent Pos: {env.agent_pos}; Action: {action}, Reward: {reward}', end="\r")
+            
+            dqn_agent.remember(cur_state, action, reward, new_state, done)
+
+            dqn_agent.replay()       # internally iterates default (prediction) model
+            dqn_agent.target_train() # iterates target model
+
+            cur_state = new_state
+            if done:
+                print(f'\n Found Goal! Reward: {reward};')
+                break
 
 if __name__ == '__main__':
     main()
